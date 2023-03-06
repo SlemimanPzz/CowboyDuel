e7a5e71f19fe7f83bde1c8c2d7bc2077e2d56455
SGM2099 <sebasguti1511@comunidad.unam.mx>
2023-03-06 15:40:56 -0600

    Pantalla inicial y boton para iniciar el duelo

--- app/src/main/java/mx/unam/fciencias/cowboyduel/MainActivity.java
+++ app/src/main/java/mx/unam/fciencias/cowboyduel/MainActivity.java
@@ -3,6 +3,7 @@ package mx.unam.fciencias.cowboyduel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+    }
+
+    public void countdown(View botonInicio){
+        botonInicio.setVisibility(View.INVISIBLE);
+        // Agregar funcion de contar pasos
+    }
+
 }
